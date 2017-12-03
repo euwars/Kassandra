f7c081b6d2b56d696bb88124b7778a35e7737410
Farzad <nazifi10@gmail.com>
2017-12-03 22:01:46 +0100

    +

--- Sources/Kassandra/Kassandra.swift
+++ Sources/Kassandra/Kassandra.swift
@@ -22,57 +22,66 @@ import Dispatch
 public class Kassandra {
     
     public var delegate: KassandraDelegate? = nil
-
+    
     internal var socket: Socket?
-
+    
     internal var readQueue: DispatchQueue
     internal var writeQueue: DispatchQueue
     
     internal var buffer: Data
     
     internal var map = [UInt16: (Result) -> Void]()
-
-
+    
+    public var isReady: Bool {
+        if let sock = socket {
+            if sock.isActive {
+                return true
+            }
+            return false
+        }
+        return false
+    }
+    
     /**
-         Initializes a Kassandra Instance
-         
-         - Parameters
-            - host:             String denoting the server host - defaults to localhost
-            - port:             Int32 denoting the server port  - defaults to 9042
-            - authentication:   Username/Password tuple denoting user to connect with
-            - cqlVersion:       String representation of vql version to use (supported: >3.0.0)
-            - connect:          Whether to automatically connect or not
-
-         Returns a QueryResult Enum through the callback
+     Initializes a Kassandra Instance
+     
+     - Parameters
+     - host:             String denoting the server host - defaults to localhost
+     - port:             Int32 denoting the server port  - defaults to 9042
+     - authentication:   Username/Password tuple denoting user to connect with
+     - cqlVersion:       String representation of vql version to use (supported: >3.0.0)
+     - connect:          Whether to automatically connect or not
+     
+     Returns a QueryResult Enum through the callback
      
      */
     public init(host: String = "localhost", port: Int32 = 9042, using authentication: (username: String, password: String)? = nil, cqlVersion: String = "3.2.0") {
-
+        
         config.setHostAndPort(host: host, port: port)
         config.setCQLVersion(cqlVersion: cqlVersion)
         
         if let auth = authentication {
             config.setAuth(PlainText(username: auth.username, password: auth.password))
         }
-
+        
         socket = nil
         
         buffer = Data()
-
+        
         readQueue = DispatchQueue(label: "read queue", attributes: DispatchQueue.Attributes.concurrent)
         writeQueue = DispatchQueue(label: "write queue", attributes: DispatchQueue.Attributes.concurrent)
-
+        
     }
- 
+    
     /**
-         Connects to a Kassandra Server
-         
-         - Parameters
-            - keyspace      : Optional keyspace to automatically conenct to
-            - compression   : Compression Type to use
-            - onCompletion  : Callback function for on completion
-         
-         Returns a Result through the given callback
+     Connects to a Kassandra Server
+     
+     - Parameters
+     - keyspace      : Optional keyspace to automatically conenct to
+     - compression   : Compression Type to use
+     - onCompletion  : Callback function for on completion
+     
+     Returns a Result through the given callback
      
      */
     public func connect(with keyspace: String? = nil,
@@ -82,134 +91,133 @@ public class Kassandra {
         if socket == nil {
             socket = try! Socket.create(family: .inet6, type: .stream, proto: .tcp)
         }
-
+        
         guard let sock = socket else {
             throw ErrorType.GenericError("Could not create a socket")
-
         }
-
+        
         do {
             config.compression = compression
-
+            
             try sock.connect(to: config.host, port: config.port)
             
             let id = UInt16.random
             
-                self.map[id] =  { result in
-                    
-                    if let space = keyspace {
-                        result.success ? self.execute("USE \(space)", oncompletion: oncompletion) : oncompletion(Result.error(ErrorType.ConnectionError))
-                    } else {
-                        oncompletion(result)
-                    }
+            self.map[id] =  { result in
+                
+                if let space = keyspace {
+                    result.success ? self.execute("USE \(space)", oncompletion: oncompletion) : oncompletion(Result.error(ErrorType.ConnectionError))
+                } else {
+                    oncompletion(result)
                 }
+            }
             
             try Request.startup(options: config.options).write(id: id, writer: sock)
             
             config.connection = self
-
+            
         } catch {
             oncompletion(Result.error(ErrorType.ConnectionError))
             return
         }
         
         read()
-
+        
     }
-
-    /**
-        Authenticate your cassandra instance using a username/password
-            
-            ** Works with Cassandra PasswordAuthenticator
-     
-         - Parameters
-             - username      : Username for Cassandra Database
-             - password      : Password for Cassandra Database
-             - onCompletion  : Callback function for on completion
     
-        Result will either be an authSuccess on success or an authChallenge
-    */
+    /**
+     Authenticate your cassandra instance using a username/password
+     
+     ** Works with Cassandra PasswordAuthenticator
+     
+     - Parameters
+     - username      : Username for Cassandra Database
+     - password      : Password for Cassandra Database
+     - onCompletion  : Callback function for on completion
+     
+     Result will either be an authSuccess on success or an authChallenge
+     */
     public func authenticate(username: String, password: String, oncompletion: @escaping ((Result) -> Void)) {
         authenticate(with: PlainText(username: username, password: password), oncompletion: oncompletion)
     }
-
+    
     internal func authenticate(with auth: Authenticator, oncompletion: @escaping ((Result) -> Void)) {
         executeHandler(.authResponse(with: auth), oncompletion: oncompletion)
     }
-
+    
     /**
-        Executes a Create Index Query
+     Executes a Create Index Query
      
-        - Parameters
-            - table         : Name of the table to place index on
-            - field         : Name of the field to place index on
-            - onCompletion  : Callback function for on completion
+     - Parameters
+     - table         : Name of the table to place index on
+     - field         : Name of the field to place index on
+     - onCompletion  : Callback function for on completion
      
-        Returns a Result Enum through the given callback
-
-    */
+     Returns a Result Enum through the given callback
+     
+     */
     public func create(in table: String, on field: String, oncompletion: @escaping ((Result) -> Void)) {
         self.execute("CREATE INDEX ON \(table)(\(field))", oncompletion: oncompletion)
     }
-
+    
     /**
-        Executes a Create Keyspace Query
-        
-         - Parameters
-            - keyspace      : Name of the keyspace to create
-            - strategy      : Type of Replication Strategy to Use
-            - isDurable     : Optional parameter for `Durable_Writes,` defaults to true
-            - ifNotExists   : Optional parameter to add "IF NOT EXISTS"
-            - onCompletion  : Callback function for on completion
-         
-         Returns a Result Enum through the given callback
+     Executes a Create Keyspace Query
+     
+     - Parameters
+     - keyspace      : Name of the keyspace to create
+     - strategy      : Type of Replication Strategy to Use
+     - isDurable     : Optional parameter for `Durable_Writes,` defaults to true
+     - ifNotExists   : Optional parameter to add "IF NOT EXISTS"
+     - onCompletion  : Callback function for on completion
+     
+     Returns a Result Enum through the given callback
      */
     public func create(keyspace: String, with strategy: ReplicationStrategy, isDurable: Bool = true, ifNotExists: Bool = false, oncompletion: @escaping ((Result) -> Void)) {
         self.execute("CREATE KEYSPACE \(ifNotExists ? "IF NOT EXISTS" : "") \(keyspace) WITH \(strategy) AND DURABLE_WRITES = \(isDurable);", oncompletion: oncompletion)
     }
     
     /**
-         Executes a String Representation of a CSQL Query
-         
-         - Parameters
-            - query:        String Representation of a CSQL Query to be executed
-            - onCompletion: Callback function for on completion
-         
-         Returns a Result Enum through the given callback
+     Executes a String Representation of a CSQL Query
+     
+     - Parameters
+     - query:        String Representation of a CSQL Query to be executed
+     - onCompletion: Callback function for on completion
+     
+     Returns a Result Enum through the given callback
      */
     public func execute(_ query: String, oncompletion: @escaping ((Result) -> Void)) {
         let request = Request.query(using: Raw(query: query))
         executeHandler(request, oncompletion: oncompletion)
     }
-
-
+    
+    
     /**
-         Executes a String Representation of a CSQL Query
-         
-         - Parameters
-            - query:        String Representation of a CSQL Query to be executed
-            - onCompletion: Callback function for on completion
-         
-         Returns a Result Enum through the given callback
+     Executes a String Representation of a CSQL Query
+     
+     - Parameters
+     - query:        String Representation of a CSQL Query to be executed
+     - onCompletion: Callback function for on completion
+     
+     Returns a Result Enum through the given callback
      */
     public func execute(_ request: Request, oncompletion: @escaping ((Result) -> Void)) {
         executeHandler(request, oncompletion: oncompletion)
     }
-
-
+    
+    
     /**
-         Executes a String Representation of a CSQL Query
-         
-         - Parameters
-            - query:        String Representation of a CSQL Query to be executed
-            - onCompletion: Callback function for on completion
-         
-         Returns a Result Enum through the given callback
+     Executes a String Representation of a CSQL Query
+     
+     - Parameters
+     - query:        String Representation of a CSQL Query to be executed
+     - onCompletion: Callback function for on completion
+     
+     Returns a Result Enum through the given callback
      */
     public func execute(_ query: Query, oncompletion: @escaping ((Result) -> Void)) {
         executeHandler(.query(using: query), oncompletion: oncompletion)
     }
-
+    
     private func executeHandler(_ request: Request, oncompletion: @escaping ((Result) -> Void)) {
         guard let sock = socket else {
             oncompletion(Result.error(ErrorType.GenericError("Socket Not Connected")))
@@ -221,7 +229,7 @@ public class Kassandra {
                 let id = UInt16.random
                 
                 self.map[id] = oncompletion
-
+                
                 try request.write(id: id, writer: sock)
                 
             } catch {
@@ -234,15 +242,15 @@ public class Kassandra {
 extension Kassandra {
     
     /**
-         Initialize a configuration using a `CA Certificate` directory.
-         
-         *Note:* `caCertificateDirPath` - All certificates in the specified directory **must** be hashed using the `OpenSSL Certificate Tool`.
-         
-         - Parameters:
-             - caCertificateDirPath:		Path to a directory containing CA certificates. *(see note above)*
-             - certificateFilePath:		Path to the PEM formatted certificate file. If nil, `certificateFilePath` will be used.
-             - keyFilePath:				Path to the PEM formatted key file (optional). If nil, `certificateFilePath` is used.
-             - selfSigned:				True if certs are `self-signed`, false otherwise. Defaults to true.
+     Initialize a configuration using a `CA Certificate` directory.
+     
+     *Note:* `caCertificateDirPath` - All certificates in the specified directory **must** be hashed using the `OpenSSL Certificate Tool`.
+     
+     - Parameters:
+     - caCertificateDirPath:        Path to a directory containing CA certificates. *(see note above)*
+     - certificateFilePath:        Path to the PEM formatted certificate file. If nil, `certificateFilePath` will be used.
+     - keyFilePath:                Path to the PEM formatted key file (optional). If nil, `certificateFilePath` is used.
+     - selfSigned:                True if certs are `self-signed`, false otherwise. Defaults to true.
      
      */
     
@@ -254,13 +262,13 @@ extension Kassandra {
     }
     
     /**
-         Initialize a configuration using a `Certificate Chain File`.
-         
-         *Note:* If using a certificate chain file, the certificates must be in PEM format and must be sorted starting with the subject's certificate (actual client or server certificate), followed by intermediate CA certificates if applicable, and ending at the highest level (root) CA.
-         
-         - Parameters:
-             - chainFilePath:			Path to the certificate chain file (optional). *(see note above)*
-             - selfSigned:				True if certs are `self-signed`, false otherwise. Defaults to true.
+     Initialize a configuration using a `Certificate Chain File`.
+     
+     *Note:* If using a certificate chain file, the certificates must be in PEM format and must be sorted starting with the subject's certificate (actual client or server certificate), followed by intermediate CA certificates if applicable, and ending at the highest level (root) CA.
+     
+     - Parameters:
+     - chainFilePath:            Path to the certificate chain file (optional). *(see note above)*
+     - selfSigned:                True if certs are `self-signed`, false otherwise. Defaults to true.
      
      */
     public func setSSL(with ChainFilePath: String, usingSelfSignedCert: Bool) throws {
@@ -271,13 +279,13 @@ extension Kassandra {
     }
     
     /**
-         Initialize a configuration using a `CA Certificate` file.
-         
-         - Parameters:
-             - caCertificateFilePath:	Path to the PEM formatted CA certificate file.
-             - certificateFilePath:		Path to the PEM formatted certificate file.
-             - keyFilePath:				Path to the PEM formatted key file. If nil, `certificateFilePath` will be used.
-             - selfSigned:				True if certs are `self-signed`, false otherwise. Defaults to true.
+     Initialize a configuration using a `CA Certificate` file.
+     
+     - Parameters:
+     - caCertificateFilePath:    Path to the PEM formatted CA certificate file.
+     - certificateFilePath:        Path to the PEM formatted certificate file.
+     - keyFilePath:                Path to the PEM formatted key file. If nil, `certificateFilePath` will be used.
+     - selfSigned:                True if certs are `self-signed`, false otherwise. Defaults to true.
      
      */
     public func setSSL(with CACertificatePath: String?, using CertificateFile: String?, with KeyFile: String?, selfSignedCerts: Bool) throws {
@@ -290,7 +298,7 @@ extension Kassandra {
     }
     
     private func setSSL(_ SSLConfig: SSLService.Configuration) throws {
-
+        
         config.SSLConfig = SSLConfig
         
         socket?.delegate = try SSLService(usingConfiguration: SSLConfig)
@@ -320,21 +328,22 @@ extension Kassandra {
             if let d = bytes {
                 
                 self.buffer.append(d, count: d.count)
-
+                
                 if self.buffer.count >= 9 {
                     self.unpack()
                 } else if self.buffer.count == 0 {
                     sock.close()
+                    self.socket = nil
                 }
                 
                 self.read()
             }
         }
     }
-
+    
     private func unpack() {
         while buffer.count >= 9 {
-
+            
             //Unpack header
             let flags       = UInt8(buffer[1])
             let streamID    = UInt16(msb: buffer[3], lsb: buffer[2])
@@ -347,16 +356,16 @@ extension Kassandra {
             if bytesNeeded < 0 {
                 return
             }
-
+            
             let body = buffer.subdata(in: Range(9..<9 + bodyLength))
-
+            
             buffer = buffer.subdata(in: Range(9 + bodyLength..<buffer.count))
-
+            
             handle(id: streamID, flags: flags, Result(opcode: opcode, body: body))
-        
+            
         }
     }
-
+    
     private func handle(id: UInt16, flags: Byte, _ response: Result) {
         switch response {
         case .event(let event)       : delegate?.didReceiveEvent(event: event)
@@ -366,9 +375,9 @@ extension Kassandra {
                 map[id]?(response)
                 return
             }
-
+            
             self.authenticate(with: auth, oncompletion: onCompletion)
-
+            
         case .authChallenge(_)      : break
         default                     : map[id]?(response)
         }
@@ -380,13 +389,13 @@ extension Kassandra {
     subscript(_ database: String) -> Bool {
         do {
             let request = Raw(query: "USE \(database);")
-
+            
             let r = Request.query(using: request)
             
             if let s = socket {
                 try r.write(id: 0, writer: s)
             }
-
+            
         } catch {
             return false
         }
@@ -394,3 +403,4 @@ extension Kassandra {
     }
     
 }
+
